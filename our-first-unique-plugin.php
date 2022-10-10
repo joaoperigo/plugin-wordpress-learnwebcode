7ed022cc46b6d49283a8b6c05fd0cfa0eac5dde1
João Cabral Périgo <joao.perigo@devio.com.br>
2022-10-10 03:03:54 -0300

    Concatenate info after the_content()

--- our-first-unique-plugin.php
+++ our-first-unique-plugin.php
@@ -4,6 +4,12 @@
     Plugin Name: Our Test Plugin
     Description: A Truly amazing plugin
     Version: 1.0
-    Author: Brad
+    Author: Joao
     Author URI: https://zapiens.com.br
 */
+
+add_filter('the_content', 'addToEndOfPost');
+
+function addToEndOfPost($content) {
+    return $content . '<p>My name is Joao</p>'; // concatenate to add this <p> after content besides substitute it
+}
